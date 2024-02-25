e74923b28c761bc436fb3499e72e5e7fdfffe0d5
Nathaniel Watts <reg@nathanielwatts.com>
2024-02-24 23:21:05 -0600

    Add/configure indent-blankline

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,15 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = {
+      -- scope keeps one of the lines highlighted, depending on the cursor position
+      -- I ultimately didn't really like how this looked, visually, so I'm disabling it
+      enabled = false
+    }
+  }
+}
