807faf2e521bf298fc25e3e87044199f8f341bb2
Nathaniel Watts <reg@nathanielwatts.com>
2024-02-24 22:59:03 -0600

    Add git-messenger

---
+++ nvim/lua/plugins/git-messenger.lua
@@ -0,0 +1,11 @@
+return {
+  "rhysd/git-messenger.vim",
+  lazy = false,
+  keys = {
+    {
+      "<Leader>gm",
+      "<cmd>GitMessenger<CR>",
+      desc = "Show the commit message for the current line",
+    }
+  }
+}
