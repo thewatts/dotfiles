50806b6b278c2f08e619971007952914f35320d7
Nathaniel Watts <reg@nathanielwatts.com>
2024-02-25 00:20:27 -0600

    Formatting of Neotree.lua file

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -2,10 +2,10 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
-    "nvim-lua/plenary.nvim", -- 3rd party "standard library" functions
+    "nvim-lua/plenary.nvim",       -- 3rd party "standard library" functions
     "nvim-tree/nvim-web-devicons", -- for fancy icons :)
-    "MunifTanjim/nui.nvim", -- ui component library for neovim
-    "s1n7ax/nvim-window-picker", -- This plugins prompts the user to pick a window and returns the window id of the picked window
+    "MunifTanjim/nui.nvim",        -- ui component library for neovim
+    "s1n7ax/nvim-window-picker",   -- This plugins prompts the user to pick a window and returns the window id of the picked window
   },
   keys = {
     {
