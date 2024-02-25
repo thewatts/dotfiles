e8d1a8ad5f05bf5563056697737719e02cbe3abe
Nathaniel Watts <reg@nathanielwatts.com>
2024-02-24 23:24:56 -0600

    Add/configure nvim-treesitter
    Note: If using with `vim-rails`, and having `highlight` enabled - it can
    cause some issues, without the `additional_vim_regex_highlighting`
    enabled.
    
    Reference: https://github.com/nvim-treesitter/nvim-treesitter/issues/6187

---
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        sync_install = false,
+        highlight = {
+          enable = true,
+          -- https://github.com/nvim-treesitter/nvim-treesitter/issues/6187
+          additional_vim_regex_highlighting = true,
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
+}
