22d4c444b21210b78ad64c3a208582a67121660f
Nathaniel Watts <reg@nathanielwatts.com>
2024-02-26 10:45:25 -0600

    Remove (non-existent) linter for html
    Given the list of linters on the nvim-lint project, `prettier` isn't one
    that's available :)

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -11,7 +11,6 @@ return {
 
     lint.linters_by_ft = {
       eruby = { "erb_lint" },
-      html = { "prettier" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
