ca4aaccfe37c64029943aece8bf6746fcf2d7628
Nathaniel Watts <reg@nathanielwatts.com>
2024-02-26 12:36:56 -0600

    Move plugins under watts, update init.lua
    Reference example: https://github.com/josean-dev/dev-environment-files/tree/main/.config/nvim/lua/josean

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,4 @@
 require("watts.core")
 require("watts.git")
 require("watts.ruby")
-
--- lazy.nvim is a modern plugin manager for Neovim.
--- https://github.com/folke/lazy.nvim?tab=readme-ov-file#-installation
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
--- From the docs:
--- require("lazy").setup(plugins, opts)
-require("lazy").setup({ { import = "plugins" } }, {
-  change_detection = {
-    notify = false
-  }
-})
-
--- Launch Lazy with <leader>l
-vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { noremap = true })
+require("watts.lazy")
