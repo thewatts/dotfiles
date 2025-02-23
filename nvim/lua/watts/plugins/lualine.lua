d469e21bf9e502997a96861e88cc40fa2eea8cda
Nathaniel Watts <1141717+thewatts@users.noreply.github.com>
2025-02-23 14:04:50 -0600

    Update lualine config to emphasize filename/path
    It's always bugged me that the full filepath hasn't shown in the output.
    BUT - there's the issue as well of, what happens if there's a long git
    branch name...
    
    This updates things to enable filepath, remove some other things, and
    keep the branch name to only take up a max of 8% of the window width,
    but also show the end of it (not the beginning)
    
    Ex:
    
    Branch name: "my-neat-branch"
    
    `...neat-branch`
    
    instead of
    
    `my-neat-b...`

--- nvim/lua/watts/plugins/lualine.lua
+++ nvim/lua/watts/plugins/lualine.lua
@@ -1,11 +1,31 @@
--- A blazing fast and easy to configure neovim statusline plugin written in pure lua.
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function() -- Switch to config function for dynamic loading
+  config = function()
     require('lualine').setup({
       options = {
-        theme = require('nostromo').lualine
+        theme = require('nostromo').lualine,
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          {
+            'branch',
+            fmt = function(str)
+              local max_width = math.floor(vim.o.columns * 0.08) -- 8% of window width
+
+              if #str > max_width then
+                return '…' .. str:sub(-max_width + 1) -- Show last part of branch name with an ellipsis
+              end
+
+              return str
+            end
+          }
+        },
+        lualine_c = { { 'filename', path = 1 } }, -- Show relative path (use path = 2 for absolute)
+        lualine_x = { 'filetype' },               -- OS display is now removed
+        lualine_y = {},                           -- Removed progress %
+        lualine_z = { 'location' }
       }
     })
   end
