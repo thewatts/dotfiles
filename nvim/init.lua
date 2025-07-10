ca9e1776bbd0d409d31805db78c10e50f1c6dd9a
Nathaniel Watts <nathaniel.watts@huntresslabs.com>
2025-07-10 18:57:48 -0500

    Add Markdown file settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,12 @@ require("watts.core")
 require("watts.git")
 require("watts.ruby")
 require("watts.lazy")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.textwidth = 0 -- Set to 0 to wrap at window width
+  end,
+})
