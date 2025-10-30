d68e902c4901fc8b4680c899f301652264addeeb
Nathaniel Watts <hello@nathanielwatts.com>
2025-10-30 14:03:36 -0500

    Add embedded-template for treesitter

--- nvim/lua/watts/plugins/nvim-treesitter.lua
+++ nvim/lua/watts/plugins/nvim-treesitter.lua
@@ -12,7 +12,7 @@ return {
       parser_config.haml = {
         install_info = {
           url = "https://github.com/vitallium/tree-sitter-haml",
-          files = {"src/parser.c", "src/scanner.c"},
+          files = { "src/parser.c", "src/scanner.c" },
           branch = "main",
         },
         filetype = "haml",
@@ -36,4 +36,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-context",
   },
+  {
+    "tree-sitter/tree-sitter-embedded-template"
+  }
 }
